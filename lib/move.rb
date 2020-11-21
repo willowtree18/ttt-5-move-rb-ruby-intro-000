6bb2b4de91a785ba52a6fbc7714b9572a66f3195
Emily Willow <emily.shapiera@gmail.com>
2020-11-21 21:07:13 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,6 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+display_board(board)
 
 # code your input_to_index and move method here!
 def input_to_index(user_input)
