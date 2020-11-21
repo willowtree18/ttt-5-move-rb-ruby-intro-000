c8bd910498bb629e3a56ff03d032ca56486826cc
Emily Willow <emily.shapiera@gmail.com>
2020-11-21 21:10:53 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,7 @@ display_board(board)
 # code your input_to_index and move method here!
 def input_to_index(user_input)
   user_input.to_i - 1
+end
 
 def move(board, index, player = "X")
   board[index] = player
