7544245cde1e0cf381af88cb3288bb2be429d1d3
Emily Willow <emily.shapiera@gmail.com>
2020-11-21 20:39:05 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,7 +6,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+input_to_index
 # code your input_to_index and move method here!
 def update_array_at_with(array, index, value)
   array[index] = value
 end
+
